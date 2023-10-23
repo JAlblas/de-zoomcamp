b06829419b1de9ced5dea68ee71522e72a2e8762
Jasper Alblas <just_jasper@hotmail.com>
2023-10-23 15:04:23 +0200

    Added dag to fetch green taxi data

---
+++ week_2/homework/dags/ingestion_green_taxi.py
@@ -0,0 +1,56 @@
+import os
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+from upload_gcs import upload_to_gcs
+
+
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+# BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
+# PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+BUCKET = os.environ.get("GCP_GCS_BUCKET")
+
+URL_PREFIX = "https://d37ci6vzurychx.cloudfront.net/trip-data"
+DL_URL = URL_PREFIX + \
+    '/green_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
+OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + \
+    '/output_green_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
+# TABLE_NAME_TEMPLATE = 'yellow_taxi_{{ execution_date.strftime(\'%Y_%m\') }}'
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "retries": 1,
+}
+
+# NOTE: DAG declaration - using a Context Manager (an implicit way)
+with DAG(
+    dag_id="ingestion_green_taxi_v01",
+    schedule_interval="0 6 2 * *",
+    start_date=datetime(2019, 1, 1),
+    end_date=datetime(2021, 1, 1),
+    default_args=default_args,
+    catchup=True,
+    max_active_runs=3,
+    tags=['de-taxi'],
+) as dag:
+
+    fetch_data = BashOperator(
+        task_id="fetch_data_green_taxi",
+        bash_command=f'curl -sSLf {DL_URL} > {OUTPUT_FILE_TEMPLATE}'
+    )
+
+    local_to_gcs = PythonOperator(
+        task_id="green_taxi_to_gcs",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": "raw/green_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet",
+            "local_file": OUTPUT_FILE_TEMPLATE
+        },
+    )
+
+    fetch_data >> local_to_gcs
